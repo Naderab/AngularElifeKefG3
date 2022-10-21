9646935562b2f9ffbab203e54f0ae5be750f8ff6
Naderab <abdellaouinader@gmail.com>
2022-10-21 15:13:07 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/core/Product';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  title:string = "Hello to products page";
+  products:Product[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+    this.products = [
+      {id: '1', title: "T-shirt 1", price: 18, quantity: 0, like: 0},
+      {id: '2', title: "T-shirt 2", price: 21, quantity: 10, like: 0},
+      {id: '3', title: "T-shirt 3", price: 16, quantity: 8, like: 0}, ]
+  }
+
+  Buy(id:string) {
+    this.products.map((p)=>{return p.id === id && p.quantity--;})
+  }
+
+  Like(id:string) {
+    this.products.map((p)=>{return p.id === id && p.like++;})
+
+  }
+
+}
